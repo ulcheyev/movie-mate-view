d655cf35a70ac522de6b38875ddecf85a97c05e4
Yevgeniy <ulchenkov2001@gmail.com>
2025-01-05 23:53:10 +0100

    Add dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/movie-mate-view",
   plugins: [react()],
   resolve: {
     alias: {
